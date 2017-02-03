1d2c838c5562ae8f852c1cc3603f0bae4d81c034
Gilberto Vieira da Silva <gilberto.silva@tecsinapse.com.br>
2017-02-03 13:42:20 -0200

    Criando um componente

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
-    return <h1>Hello World</h1>
+    //return <h1>Hello World</h1>
+    // OR
+    return React.createElement('h1', null, 'Hello World');
   }
 }
 
