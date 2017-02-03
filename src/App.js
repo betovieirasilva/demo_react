89b4565cd090b149bb57036a121df4f63201e78b
Gilberto Vieira da Silva <gilberto.silva@tecsinapse.com.br>
2017-02-03 13:44:01 -0200

    Criando um componente

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-class App extends React.Component {
-  render(){
-    //return <h1>Hello World</h1>
-    // OR
-    return React.createElement('h1', null, 'Hello World');
-  }
-}
+//class App extends React.Component {
+//  render(){
+//    return <h1>Hello World</h1>
+//  }
+//}
 
+//OR
+const App = () => <h1>Hello World</h1>
 export default App
