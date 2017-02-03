e8737625f3ab6cc05c135ae21377570af89a46ab
Gilberto Vieira da Silva <gilberto.silva@tecsinapse.com.br>
2017-02-03 13:50:32 -0200

    Criando um componente. Usar (.... ) https://egghead.io/lessons/react-the-render-method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-//class App extends React.Component {
-//  render(){
-//    return <h1>Hello World</h1>
-//  }
-//}
+class App extends React.Component {
+  render(){
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <b>Bold</b>
+        </div>
+    )
+  }
+}
 
 //OR
-const App = () => <h1>Hello World</h1>
+//const App = () => <h1>Hello World</h1>
 export default App
