363ed54b67833e57b55985d556bfc124a408fd06
Gilberto Vieira da Silva <gilberto.silva@tecsinapse.com.br>
2017-02-14 16:33:55 -0200

    Criado o AppB apenas para isolar mais alguns testes sem apagar os exemplos anteriores

---
+++ src/AppB.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AppB extends React.Component {
+
+  render() {
+    return (
+        <div>
+            <h1>Novos testes de uso do React</h1>
+        </div>
+    )
+  }
+}
+
+export default AppB
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+//
+// ReactDOM.render(
+//   <App cat={5} txt="this is propertie value" />,
+//   document.getElementById('root')
+// );
+
+//novos testes com react, para facilitar isolei no AppB
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AppB from './AppB';
 
 ReactDOM.render(
-  <App cat={5} txt="this is propertie value" />,
-  document.getElementById('root')
+    <AppB />,
+    document.getElementById('root')
 );
