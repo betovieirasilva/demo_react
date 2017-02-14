80c3a1c219ab5cd33abfb9e4fc62bc758b6c7477
Gilberto Vieira da Silva <gilberto.silva@tecsinapse.com.br>
2017-02-14 12:14:05 -0200

    https://egghead.io/lessons/react-state-basics - Add method for update value

--- src/App.js
+++ src/App.js
@@ -9,12 +9,17 @@ class App extends React.Component {
       }
   }
 
+  update ( e ) {
+      this.setState({txt: e.target.value})
+  }
+
   render(){
     let txt = this.props.txt
     return (
         <div>
             <h1>{txt}</h1>
             <br />
+            <input type="text" onChange={this.update.bind(this)} />
             <h1>{this.state.txt}</h1>
         </div>
     )
