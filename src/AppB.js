c5458d7cdf3465ee12837c53edabf7181f58711a
Gilberto Vieira da Silva <gilberto.silva@tecsinapse.com.br>
2017-02-14 16:39:52 -0200

    https://egghead.io/lessons/react-component-lifecycle-mounting-usage Adicionado código inicial do demo

--- src/AppB.js
+++ src/AppB.js
@@ -1,14 +1,57 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class AppB extends React.Component {
 
-  render() {
-    return (
-        <div>
-            <h1>Novos testes de uso do React</h1>
-        </div>
-    )
-  }
+    constructor(){
+        super();
+        this.state = {
+            val: 0
+        }
+        this.update = this.update.bind(this)
+    }
+
+    update () {
+        this.setState({val: this.state.val + 1})
+    }
+
+    componentWillMount(){
+        console.log('mount');
+    }
+
+    render() {
+        console.log('render');
+        return (
+            <div>
+                <button onClick={this.update}>{this.state.val}</button>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        console.log('did mount');
+    }
+    componentWillUnmount() {
+        console.log('unmount');
+    }
 }
 
-export default AppB
+class Wrapper extends React.Component {
+    mount() {
+        ReactDOM.render(<AppB />, document.getElementById('a'))
+    }
+    unmount(){
+        ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+    }
+    render(){
+        return (
+            <div>
+                <button onClick={this.mount.bind(this)}>Mount</button>
+                <button onClick={this.unmount.bind(this)}>UnMount</button>
+                <div id="a"></div>
+            </div>
+        )
+    }
+}
+
+export default Wrapper
