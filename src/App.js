4ef9ea931d41196ca21d02a2066f8fdf62dac5dd
Gilberto Vieira da Silva <gilberto.silva@tecsinapse.com.br>
2017-02-14 10:58:34 -0200

    https://egghead.io/lessons/react-introduction-to-properties

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ class App extends React.Component {
   render(){
     return (
         <div>
-            <h1>Hello World</h1>
-            <b>Bold</b>
+            <h1>{this.props.txt}</h1>
         </div>
     )
   }
